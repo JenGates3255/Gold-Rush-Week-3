0d522b3ac9a4a866fb18aef8baf86abdccfcc82f
Timothy Neumann <timothyneumann1@gmail.com>
2014-06-30 15:41:13 -0600

    gold rush finished

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 $(document).on('ready', function() {
-	
-	$( '#map' ).on( 'click', function(event) {
-  		$("#map").before("pageX: " + event.pageX + ",pageY: " + event.pageY);
-   		// $('#map').append('<div class="buttons"></div>');
-
-
-
-
-   		console.log(event.pageX, event.pageY)
-
-
-
+	$( document ).on( 'click', '#map', function(event) {
+	// $('#map').append('<div class="buttons"></div>');
+   		var note=prompt('Would you like to add a note?');
+   		var style={
+   			left: event.pageX, 
+   			top: event.pageY,
+   			};
+   		//$('.button').css(style)
+		$('#map').after($('<div class="button"></div>').css(style).text(note).css('color','transparent'))
 	});
-
-
-
-
-
-  
+	$(document).on('click','.button',function(event){
+		$(this).hide();
+	});
+	$(document).on('mouseover','.button',function(event){
+		var style={
+			padding:'20px',
+			color:'black'
+		}
+		$(this).css(style)
+	})
+	$(document).on('mouseout','.button',function(event){
+		var style={
+			color:'transparent',
+			}
+		$(this).css(style)
+	})
 });
